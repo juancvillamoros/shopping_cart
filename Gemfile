d9a4e31b2a422c120ad612185beee2b032b04bda
juancvillamoros <jvillamoros@outlook.com>
2022-11-03 09:26:50 -0500

    costumize the controllers

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
-
+gem 'tailwindcss-rails', '~> 2.0', '>= 2.0.17'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,46 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+
+    def new 
+        @product = Product.new
+    end
+    def create
+        @product = Product.new(params[:product])
+        if @product.save
+            flash[:notice] = "Successfully created product."
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit 
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update_attributes(params[:product])
+            redirect_to products_params
+            flash[:notice] = "Successfully updated product."
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        flash[:notice] = "Successfully destroyed product."
+        redirect_to products_path
+    end
+
+    private
+        def product_params
+            params.require(:product).permit(:title, :price)
+        end
 end
